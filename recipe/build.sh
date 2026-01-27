a0db0fbd307424fdaef2cce47ffdbdb2ab2e53c8
Rui Xue <rx.astro@gmail.com>
2026-01-26 22:06:47 -0600

    Remove `cfitsio` as explicit dependency; architecture-specific `-Darchflag` for macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,9 +11,19 @@ cmake_args=(
 if [ $(uname) = Darwin ] ; then
     linkflags="-Wl,-rpath,$PREFIX/lib $LDFLAGS"
 
+    if [[ "${target_platform}" == "osx-arm64" ]]; then
+      cmake_args+=(
+          -Darch=darwin64
+          -Darchflag=arm64
+      )
+    else
+      cmake_args+=(
+          -Darch=darwin64
+          -Darchflag=x86_64
+      )
+    fi
+
     cmake_args+=(
-        -Darch=darwin64
-        -Darchflag=x86_64
         -DCMAKE_Fortran_COMPILER=$FC
         # Make sure to get Conda versions of libraries:
         -DLIBXML2_ROOT_DIR=$PREFIX
